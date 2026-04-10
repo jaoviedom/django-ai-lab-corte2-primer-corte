16e23e915d7bdf0847e019ddc6a13b9ff6e454e0
Jose Oviedo <33611065+jaoviedom@users.noreply.github.com>
2026-04-10 00:09:13 +0000

    Implementar vistas y formularios para la gestión de productos y pedidos en la aplicación de tienda

---
+++ tienda/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+app_name = "tienda"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("productos/",views.lista_productos, name="lista_productos"),
+    path("productos/<int:pk>/", views.detalle_producto, name="detalle_producto"),
+    path("productos/nuevo/", views.crear_producto, name="crear_producto"),
+    path("productos/<int:pk>/editar/", views.editar_producto, name="editar_producto"),
+    path("productos/<int:pk>/eliminar/", views.eliminar_producto, name="eliminar_producto"),
+    path("pedidos/", views.lista_pedidos, name="lista_pedidos"),
+    path("pedidos/<int:pk>", views.detalle_pedido, name="detalle_pedido"),
+    path("clientes/<int:pk>", views.detalle_cliente, name="detalle_cliente"),
+]
--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,89 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Producto, Pedido, Cliente
+from .forms import ProductoForm
 
-# Create your views here.
+'''
+Vista de inicio
+(Solo muestra una plantilla básica sin datos)
+'''
+def home(request):
+    return render(request, "tienda/home.html", {})
+
+'''
+Vista para listar los productos
+'''
+def lista_productos(request):
+    productos = Producto.objects.all().order_by("nombre")
+    return render(request, "tienda/lista_productos.html", {"productos": productos})
+
+'''
+Vista de detalle de un producto
+'''
+def detalle_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, "tienda/detalle_producto.html", {"producto": producto})
+
+'''
+Vista que lista todos los pedidos
+'''
+def lista_pedidos(request):
+    pedidos = Pedido.objects.select_related("cliente").prefetch_related("productos").order_by("-fecha")
+    return render(request, "tienda/lista_pedidos.html", {"pedidos": pedidos})
+
+'''
+Vista de detalle del pedido
+'''
+def detalle_pedido(request, pk):
+    pedido = get_object_or_404(
+        Pedido.objects.select_related("cliente").prefetch_related("productos"),
+        pk=pk
+    )
+    return render(request, "tienda/detalle_pedido.html", {"pedido": pedido})
+
+'''
+Vista de detalle de un cliente
+'''
+def detalle_cliente(request, pk):
+    cliente = get_object_or_404(cliente, pk=pk)
+    pedidos = cliente.pedidos.select_related("cliente").prefetch_related("productos"). order_by("-fecha")
+    return render(
+        request, 
+        "tienda/detalle_cliente.html", 
+        {
+            "cliente": cliente,
+            "pedidos": pedidos
+        }
+    )
+
+def crear_producto(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("tienda:lista_productos")
+    else:
+        form = ProductoForm()
+    
+    return render(request, "tienda/crear_producto.html", {"form": form})
+
+def editar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+
+    if request.method == "POST":
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect("tienda:detalle_producto", pk=producto.pk)
+    else:
+        form = ProductoForm(instance=producto)
+    
+    return render(request, "tienda/editar_producto.html", {"form": form, "producto": producto})
+
+def eliminar_producto(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+
+    if request.method == "POST":
+        producto.delete()
+        return redirect("tienda:lista_productos")
+    
+    return render(request, "tienda/eliminar_producto.html", {"producto": producto})
